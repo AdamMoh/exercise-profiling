de7340a90478d300a0bea81cc8e7183b297965e3
AdamMoh <xlkartu12345@gmail.com>
2024-03-13 16:42:27 +0700

    [REFACTOR] optimizing method joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -2,7 +2,6 @@ package com.advpro.profiling.tutorial.service;
 
 import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
-import com.advpro.profiling.tutorial.repository.StudentCourseRepository;
 import com.advpro.profiling.tutorial.repository.StudentRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -10,10 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author muhammad.khadafi
@@ -26,23 +25,6 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
-    @Autowired
-    private StudentCourseRepository studentCourseRepository;
-
-//    public List<StudentCourse> getAllStudentsWithCourses() {
-//        List<Student> students = studentRepository.findAll();
-//        List<StudentCourse> studentCourses = new ArrayList<>();
-//        for (Student student : students) {
-//            List<StudentCourse> studentCoursesByStudent = studentCourseRepository.findByStudentId(student.getId());
-//            for (StudentCourse studentCourseByStudent : studentCoursesByStudent) {
-//                StudentCourse studentCourse = new StudentCourse();
-//                studentCourse.setStudent(student);
-//                studentCourse.setCourse(studentCourseByStudent.getCourse());
-//                studentCourses.add(studentCourse);
-//            }
-//        }
-//        return studentCourses;
-//    }
     public List<StudentCourse> getAllStudentsWithCourses() {
         return entityManager.createQuery(
                     "SELECT new StudentCourse(sc.student, sc.course) FROM StudentCourse sc", StudentCourse.class)
@@ -57,11 +39,9 @@ public class StudentService {
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
-        for (Student student : students) {
-            result += student.getName() + ", ";
-        }
-        return result.substring(0, result.length() - 2);
+        return students.stream()
+                .map(Student::getName)
+                .collect(Collectors.joining(", "));
     }
 }
 
